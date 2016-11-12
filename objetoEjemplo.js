3a2678c74da2de7ad099c52ea146366d8a4ba766
shadoworzil <shadow.orzil6@gmail.com>
2016-11-12 09:29:01 -0400

    2do Examen'

---
+++ objetoEjemplo.js
@@ -0,0 +1,5 @@
+var Automovil={motor:"V8",Cilindrada:4500,modelo:"2016"}
+console.log("Las caracteristicas del Automovil son: ")
+console.log("Motor: "+Automovil.motor)
+console.log("Cilindrada: "+Automovil.Cilindrada)
+console.log("modelo: "+Automovil.modelo)
---
+++ tablaSuma.js
@@ -0,0 +1,7 @@
+ var x=7;
+ var a=x;
+ console.log("suma de nuleros 7 : ");
+ for (var i = 1; i <=10; i++) {
+   ++a;
+   console.log(x +"+"+i+"="+a);
+   }
